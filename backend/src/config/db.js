14d79e7291cef78382d16ed68317470300f50cb0
jac <castillojavierb@icloud.com>
2026-07-01 17:54:49 -0500

    Usar pool de conexiones MySQL

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = await mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-console.log("✅ Conectado a MySQL");
+console.log("✅ Pool de MySQL creado");
