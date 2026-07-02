246a3339de4bd1df8b9b23c44e452f3cc35588f3
jac <castillojavierb@icloud.com>
2026-07-02 01:28:55 -0500

    Actualizar backend con createPool

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,15 @@ app.use(cors({
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
+
 
 /* =========================
    🔐 REGISTRO
