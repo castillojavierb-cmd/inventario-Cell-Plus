597707a3530711ddce8227dcc6a468f4db59611b
jac <castillojavierb@icloud.com>
2026-07-02 01:57:06 -0500

    Configurar recuperacion de contraseña y variables de entorno

--- backend/server.js
+++ backend/server.js
@@ -23,15 +23,12 @@ app.use(cors({
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
-
 
 /* =========================
    🔐 REGISTRO
